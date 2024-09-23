fcd73d1eee14711d0ff592c139ada15a0fbe05ed
DevAyush075 <153929909+DevAyush075@users.noreply.github.com>
2024-09-23 22:26:39 +0530

    Add files via upload

---
+++ signup.java
@@ -0,0 +1,14 @@
+package electricity.biling.system.splash;
+
+import com.sun.tools.javac.Main;
+
+import javax.swing.*;
+
+public class signup extends JFrame {
+    signup(){
+
+    }
+    public static void main(String[] args) {
+        new signup();
+    }
+}
